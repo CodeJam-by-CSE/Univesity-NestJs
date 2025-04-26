7b7921eee2051493992a48b0d37c6a1c173808d2
Rashmika Rathnayaka <rashmikarathnayaka01@gmail.com>
2025-04-26 19:34:35 +0530

    microservice create

--- apps/feature-detection/src/feature-detection.controller.ts
+++ apps/feature-detection/src/feature-detection.controller.ts
@@ -14,5 +14,11 @@ export class FeatureDetectionController {
     console.log('Received image for canny edge detection');
     return await this.featureDetectionService.cannyEdgeDetection(imagePath);
   }
+
+  @EventPattern({ cmd: 'harris_sharp_image' })
+  async handleHarrisSharp(data: { imagePath: string; k?: number; windowSize?: number; thresh?: number }) {
+    console.log('Received image for Harris corner detection');
+    return await this.featureDetectionService.detectCorners(data.imagePath, data.k, data.windowSize, data.thresh);
+  }
   
 }
