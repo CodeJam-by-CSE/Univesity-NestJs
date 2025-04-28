27f1e8e5d862c5984e039a5a4f0a97f01dea09f4
Aravinda-HWK <aravinda.20@cse.mrt.ac.lk>
2025-04-28 11:20:08 +0530

    Add bugs to sharpen image

--- test/run_marking.py
+++ test/run_marking.py
@@ -1,15 +1,21 @@
-from compare_images import compare_images  # assuming you saved previous function as compare_images.py
+from compare_images import compare_images
 
 def main():
     tests = [
+        # Basic Processing Tests
         ('apps/cse40/expected_images/contrast_20_image.png', 'apps/basic-processing/output_images/contrast_20_image.png', 'Contrast 20'),
         ('apps/cse40/expected_images/embossed_standard_1.0.png', 'apps/basic-processing/output_images/embossed_standard_1.0.png', 'Embossed Standard 1.0'),
         ('apps/cse40/expected_images/greyscale_image.png', 'apps/basic-processing/output_images/greyscale_image.png', 'Greyscale Image'),
         ('apps/cse40/expected_images/negative_image.png', 'apps/basic-processing/output_images/negative_image.png', 'Negative Image'),
         ('apps/cse40/expected_images/resized_image.png', 'apps/basic-processing/output_images/resized_image.png', 'Resized Image'),
         ('apps/cse40/expected_images/rotated_90_image.png', 'apps/basic-processing/output_images/rotated_90_image.png', 'Rotated Image'),
+        ('apps/cse40/expected_images/sharpened_image.png', 'apps/basic-processing/output_images/sharpened_image.png', 'Sharpened Image'),
+
+        # Enhancement Tests
         ('apps/cse40/expected_images/flood_filled_input_image.png', 'apps/enhancement/output_images/flood_filled_input_image.png', 'Flood Filled Input Image'),
         ('apps/cse40/expected_images/histogram_equalized.png', 'apps/enhancement/output_images/histogram_equalized.png', 'Histogram Equalized'),
+
+        # Feature Detection Tests
     ]
 
     total_marks = 0
