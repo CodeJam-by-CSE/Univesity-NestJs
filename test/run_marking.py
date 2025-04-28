1ebc160482a133bc0eddc14564ef962e6cb71314
Chanuka Lakshan Chandra Yapa <118423471+Chanuka-ChandraYapa@users.noreply.github.com>
2025-04-28 11:56:11 +0530

    add bugs to the canny edge detection

--- test/run_marking.py
+++ test/run_marking.py
@@ -1,13 +1,23 @@
-from compare_images import compare_images  # assuming you saved previous function as compare_images.py
+# assuming you saved previous function as compare_images.py
+from compare_images import compare_images
+
 
 def main():
     tests = [
-        ('apps/cse40/expected_images/contrast_20_image.png', 'apps/basic-processing/output_images/contrast_20_image.png', 'Contrast 20'),
-        ('apps/cse40/expected_images/embossed_standard_1.0.png', 'apps/basic-processing/output_images/embossed_standard_1.0.png', 'Embossed Standard 1.0'),
-        ('apps/cse40/expected_images/greyscale_image.png', 'apps/basic-processing/output_images/greyscale_image.png', 'Greyscale Image'),
-        ('apps/cse40/expected_images/negative_image.png', 'apps/basic-processing/output_images/negative_image.png', 'Negative Image'),
-        ('apps/cse40/expected_images/flood_filled_input_image.png', 'apps/enhancement/output_images/flood_filled_input_image.png', 'Flood Filled Input Image'),
-        ('apps/cse40/expected_images/histogram_equalized.png', 'apps/enhancement/output_images/histogram_equalized.png', 'Histogram Equalized'),
+        ('apps/cse40/expected_images/contrast_20_image.png',
+         'apps/basic-processing/output_images/contrast_20_image.png', 'Contrast 20'),
+        ('apps/cse40/expected_images/embossed_standard_1.0.png',
+         'apps/basic-processing/output_images/embossed_standard_1.0.png', 'Embossed Standard 1.0'),
+        ('apps/cse40/expected_images/greyscale_image.png',
+         'apps/basic-processing/output_images/greyscale_image.png', 'Greyscale Image'),
+        ('apps/cse40/expected_images/negative_image.png',
+         'apps/basic-processing/output_images/negative_image.png', 'Negative Image'),
+        ('apps/cse40/expected_images/flood_filled_input_image.png',
+         'apps/enhancement/output_images/flood_filled_input_image.png', 'Flood Filled Input Image'),
+        ('apps/cse40/expected_images/histogram_equalized.png',
+         'apps/enhancement/output_images/histogram_equalized.png', 'Histogram Equalized'),
+        ('apps/cse40/expected_images/canny_edges.png',
+         'apps/feature-detection/output_images/canny_edges.png', "Canny Edge detection")
     ]
 
     total_marks = 0
@@ -18,8 +28,9 @@ def main():
             total_marks += score
         except Exception as e:
             print(f"{name}: Error ({e})")
-    
+
     print(f"Total Marks: {total_marks}/{len(tests)*100}")
+
 
 if __name__ == "__main__":
     main()
