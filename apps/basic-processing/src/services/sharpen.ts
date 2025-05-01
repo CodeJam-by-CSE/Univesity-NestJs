0073c0e14131611c2ce9dff37631516c9aef1c9a
Aravinda-HWK <aravinda.20@cse.mrt.ac.lk>
2025-05-01 23:07:33 +0530

    Remove bugs in sharpen image

--- apps/basic-processing/src/services/sharpen.ts
+++ apps/basic-processing/src/services/sharpen.ts
@@ -30,6 +30,11 @@ export class SharpenService {
 
           for (let ky = -offset; ky <= offset; ky++) {
             for (let kx = -offset; kx <= offset; kx++) {
+              const px = Math.min(Math.max(x + kx, 0), width - 1);
+              const py = Math.min(Math.max(y + ky, 0), height - 1);
+              const kernelValue = this.strongKernel[ky + offset][kx + offset];
+              const sourceIndex = (py * width + px) * channels + c;
+              sum += imageData[sourceIndex] * kernelValue;
             }
           }
 
